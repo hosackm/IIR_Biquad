3bfecd813722d790b914c6a19de3b88101d7e142
Matt Hosack <hosack.matt@gmail.com>
2013-08-08 11:16:53 -0700

    Initial commit

---
+++ IIR_Biquad/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  IIR_Biquad
+//
+//  Created by Matthew Hosack on 8/8/13.
+//  Copyright (c) 2013 Matthew Hosack. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
